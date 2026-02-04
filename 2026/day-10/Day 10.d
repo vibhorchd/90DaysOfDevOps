576a84e4cb9acdfcc323498dc7ced0393f3b6023
vibhorchd <146221455+vibhorchd@users.noreply.github.com>
2026-02-03 20:42:45 -0500

    Create Day 10.d

---
+++ 2026/day-10/Day 10.d
@@ -0,0 +1,65 @@
+# Day 10 Challenge
+
+## Files Created
+
+drwxrwxr-x 2 ubuntu ubuntu 4096 Jan 31 21:03 vibhorsre
+-r-------- 1 ubuntu ubuntu    0 Feb  4 01:05 devops.txt
+-rwxrwxr-x 1 ubuntu ubuntu   14 Feb  4 01:19 script.sh
+-rw-r----- 1 ubuntu ubuntu   18 Feb  4 01:23 notes.txt
+drwxr-xr-x 2 ubuntu ubuntu 4096 Feb  4 01:36 project
+
+## Permission Changes
+initialluy  it was ont read wrute and execute and later it was just read
+## Commands Used
+ 92  ls
+   93  ls -ltr
+   94  cd ..
+   95  ls
+   96  touch devops.txt
+   97  ls 
+   98  echo notes.txt
+   99  ls
+  100  ls -lrt
+  101  ps -ef | grp echo
+  102  echo notes.txt
+  103  ls 
+  104  cat notes.txt
+  105  echo "notes.txt"
+  106  ls -ltr
+  107  cat notes.txt
+  108  vim script.sh
+  109  ls 
+  110  ls -lrt
+  111  ls 
+  112  clear
+  113  ls -ltr
+  114  cat script.sh
+  115  echo "this is my devops">>notes.txt
+  116  ls 
+  117  ls -lrt
+  118  head -n/etc/passwd
+  119  head -n 5/etc/passwd
+  120  head - n 5/etc/passwd
+  121  chmod400 devops.txt
+  122  sudo chmod400 devops.txt
+  123  chmod 400 devops.txt
+  124  ls -lrt
+  125  chmod 640 notes.txt
+  126  ls 
+  127  ls -lrt
+  128  chmod +x script.sh
+  129  ls -lrt
+  130  ./script.sh
+  131  ls lrt
+  132  ls -lrt
+  133  mkdir project
+  134  chmod 755 project
+  135  ls -lrt
+  136  his
+  137  history
+ubuntu@ip-172-31-30-161:~$ ^C
+ubuntu@ip-172-31-30-161:~$ 
+## What I Learned
+I learned avoy the touch  command 
+for echo we need to add input to the file tehn it works
+for the rum chmod x if we weaant to run 
